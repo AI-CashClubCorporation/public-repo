9ec0acf8180ab5b9994b54b3e8aaa103fcdcfe9a
siraphopp <44414518+siraphopp@users.noreply.github.com>
2024-05-27 15:53:35 +0700

    Update setup-lora2base-train-vast.sh

--- setup-lora2base-train-vast.sh
+++ setup-lora2base-train-vast.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo Test
+sudo apt-get update
+sudo apt-get full-upgrade -y
+sudo apt-get install nano -y
+
+git config --global credential.helper store
+git clone https://github.com/AI-CashClubCorporation/lora2base-train-vast.git
+
+cd lora2base-train-vast
